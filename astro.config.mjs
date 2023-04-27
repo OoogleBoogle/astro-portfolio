03310eb269cee44b82ec20bd5cc8fb0014c4a90b
Simon Attfield <simattfield@gmail.com>
2023-04-27 18:24:35 -0400

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), astroImageTools, frontendistahtmlMinify(), compress()],
   vite: {
     ssr: {
       // Example: Force a broken package to skip SSR processing, if needed
-      external: ['swup'],
+      external: ['swup']
     }
   }
 });
