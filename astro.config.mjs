ee0603a4b26d1735b1a017013b8bf732ac22a95a
Simon Attfield <simattfield@gmail.com>
2023-04-27 23:27:40 -0400

    Creating markdown for Projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import frontendistahtmlMinify from "@frontendista/astro-html-minify";
-
 import compress from "astro-compress";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, frontendistahtmlMinify(), compress()],
+  integrations: [tailwind(), astroImageTools, frontendistahtmlMinify(), compress(), mdx()],
   vite: {
     ssr: {
       // Example: Force a broken package to skip SSR processing, if needed
