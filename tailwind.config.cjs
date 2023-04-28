35d9bc1459a5637ce19d1e651cea75c883333fdd
Simon Attfield <simattfield@gmail.com>
2023-04-27 21:50:05 -0400

    funky cta hover

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'deep-purple': '#14081F',
-			'purple': '#60457A',
-			'almost-white': '#eed6fe',
-			'light-purple': '#A785C7',
-		},
 		fontFamily: {
 			'display': ['Comfortaa', 'cursive'],
 			'sans': ['Raleway', 'sans-serif'],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				'deep-purple': '#14081F',
+				'purple': '#60457A',
+				'almost-white': '#eed6fe',
+				'light-purple': '#A785C7',
+			},
+			animation: {
+				shine: "shine 1s",
+			},
+			keyframes: {
+				shine: {
+					"100%": { left: "125%" },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
